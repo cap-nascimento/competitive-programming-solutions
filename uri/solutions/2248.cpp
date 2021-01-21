22efda24abd4045bfcf510b1991c9a55868b11bb
Anderson Vieira do Nascimento <anascimentovieira2@gmail.com>
2021-01-21 11:40:22 -0300

    adding solution to uri 2248 - internship

--- uri/solutions/2248.cpp
+++ uri/solutions/2248.cpp
@@ -12,16 +12,47 @@
 #define nd second
 #define pb push_back
 #define mp make_pair
-#define MAX 1000010
+#define MAX 1010
 #define MOD 1000000007
 #define oo 0x7fffffff
 #define lld long long int
 #define endl '\n'
 using namespace std;
 
+bool compare(ii x, ii y){
+	if(x.nd == y.nd)
+		return x.st < y.st;
+	return x.nd > y.nd;
+}
+
 int main() {
 
 	fastio;
+
+	int n, t = 1;
+	while(cin >> n && n){
+		vector<ii> a;
+		vi codes;
+		rep(i, 0, n){
+			int c, m;
+			cin >> c >> m;
+			codes.pb(c);
+			a.pb(mp(i, m));
+		}
+		sort(a.begin(), a.end(), compare);
+		cout << "Turma " << t << endl;
+		vi ans;
+		int i = 0;
+		while(a[i].nd == a[0].nd && i < n){
+			ans.pb(codes[a[i].st]);
+			i++;
+		}
+		rep(i, 0, ans.size()){
+			cout << ans[i] << ' ';
+		}
+		cout << "\n\n";
+		t++;
+	}
 
 	return 0;
 }
