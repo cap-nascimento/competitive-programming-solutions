38846a086d41f1d1c7800edd3b3f9cfa3335f1bd
cap-nascimento <anascimentovieira2@gmail.com>
2022-06-28 06:57:40 -0300

    add piton code

--- piton.py
+++ piton.py
@@ -1,1 +1,9 @@
 # Anderson's solution
+t = int(input())
+while t > 0:
+    s = input()
+    print(s[0], end='')
+    for i in range(1, len(s)-1, 2):
+        print(s[i], end='')
+    print(s[len(s)-1])
+    t-=1
